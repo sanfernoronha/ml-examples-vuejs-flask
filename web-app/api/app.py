042353b8ef73c55aa8c0021d55b2a5959e085bfe
sanfernoronha <sanfernoronha99@gmail.com>
2020-04-13 22:53:43 +0530

    Display plots and error metrics

--- web-app/api/app.py
+++ web-app/api/app.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import cv2
+from pylab import savefig
 
 app = Flask(__name__)
 
@@ -30,11 +31,53 @@ def test2():
     target = request.form["target"]
     test_size = request.form["test_size"]
     dataset = request.files["dataset"]
-    USAhousing = pd.read_csv(dataset)
-    print(USAhousing.head())
-    X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
-               'Avg. Area Number of Bedrooms', 'Area Population']]
-    y = USAhousing['Price']
+    df = pd.read_csv(dataset)
+
+    #directory making
+    rootdirectory = name
+    parent_dir = "/home/sanfer/Documents/ml-examples-vuejs-flask/web-app/src/assets/"
+    path = os.path.join(parent_dir, rootdirectory)
+    working = path #working path
+    os.mkdir(path)
+
+    plotdirectory = "plots"
+    plot_parent_dir = parent_dir + rootdirectory + '/'
+    path = os.path.join(plot_parent_dir, plotdirectory)
+    plots_dir = path #plot path
+    os.mkdir(path)
+
+    # modeldirectory = "models"
+    # model_parent_dir = parent_dir + rootdirectory + "/"
+    # path = os.path.join(model_parent_dir, modeldirectory)
+    # model_dir = path #model path
+    # os.mkdir(path)
+
+
+
+
+
+    #pre-processiong plots
+   
+    snsdist = sns.distplot(df[target])
+    snsdist = snsdist.get_figure()
+    snsdist.savefig(plots_dir + "/dist.png")
+    snsdist.clf()
+
+    
+
+
+    dataTypes = df.dtypes
+    for items in dataTypes.iteritems():
+        # print(items)
+        # print((items[1].name))
+        if (items[1].name != 'float64' and items[1].name != 'int64'):
+            df.drop(labels=items[0], axis=1, inplace=True)
+
+    y = df[target]
+    df.drop(labels=target, axis=1, inplace=True)
+    df.replace(0, np.NaN).fillna(df.mean(), inplace=True)
+    X = df[list(df.columns)]
+   
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=float(test_size), random_state=101)
     from sklearn.linear_model import LinearRegression
@@ -46,24 +89,77 @@ def test2():
     print(coeff_df)
     
     predictions = lm.predict(X_test)
-    plt.figure()
-    plt.scatter(y_test,predictions)
-    plt.show()
-    cv2.waitKey(0)
+    # plt.figure()
+    plt.scatter(y_test, predictions)
+    plt.savefig(plots_dir + "/scatter.png")
+    plt.clf()
+
+    sn = sns.distplot((y_test - predictions), bins=50)
+    sn = sn.get_figure()
+    sn.savefig(plots_dir + "/residual.png")
+    sn.clf()
+   
+
+    # plt.show()
+    # cv2.waitKey(0)
     # sns.distplot((y_test-predictions),bins=50);
     from sklearn import metrics
     print('MAE:', metrics.mean_absolute_error(y_test, predictions))
     print('MSE:', metrics.mean_squared_error(y_test, predictions))
     print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
-
+   
     print(test_size)
     print(name)
-    return jsonify({"status": "success LinearReg","rmse":np.sqrt(metrics.mean_squared_error(y_test, predictions))}), 201
+
+    #metrics to return
+    r_square = lm.score(X, y)
+    MAE = metrics.mean_absolute_error(y_test, predictions)
+    MSE = metrics.mean_squared_error(y_test, predictions)
+    RMSE = np.sqrt(MSE)
+
+
+    #plot paths to return
+    scatterplotpath = name + "/plots/scatter.png"
+    distpath = name + "/plots/dist.png"
+    residualpath = name + "/plots/residual.png"
+
+
+    return jsonify({"status": "success LinearReg",
+    "metrics": {
+        "mae": MAE,
+        "mse": MSE,
+        "rmse": RMSE,
+        "r_square": r_square
+    },
+    "ploturl": {
+        "scatterplotpath": scatterplotpath,
+        "distpath": distpath,
+        "residualpath": residualpath
+    }
+    
+    }), 201
 
 @app.route("/api/BayReg/", methods=["POST"])
 def test3():
     name = request.form["name"]
-    print(name)
+    target = request.form["target"]
+    test_size = request.form["test_size"]
+    dataset = request.files["dataset"]
+    df = pd.read_csv(dataset)
+    dataTypes = df.dtypes
+    for items in dataTypes.iteritems():
+        # print(items)
+        # print((items[1].name))
+        if (items[1].name != 'float64' and items[1].name != 'int64'):
+            df.drop(labels=items[0], axis=1, inplace=True)
+
+    y = df[target]
+    df.drop(labels=target, axis=1, inplace=True)
+    df.replace(0, np.NaN).fillna(df.mean(), inplace=True)
+    X = df[list(df.columns)]
+    
+    
+    
     return jsonify({"status": "success Bayesian Ridge"}), 201
 
 @app.route("/api/Ridge/", methods=["POST"])
--- web-app/src/views/Home.vue
+++ web-app/src/views/Home.vue
@@ -1,93 +1,152 @@
 <template>
   <v-app>
-    <v-card class="mx-auto mt-10" width="600px">
-      <v-app-bar dark color="primary">
-        <v-toolbar-title>Train a model</v-toolbar-title>
-      </v-app-bar>
-      <v-form ref="form" v-model="valid" :lazy-validation="lazy">
-        <v-card-text>
+    <v-row no-gutters>
+      <v-col cols="12" lg="4">
+        <v-card class="ml-10 mt-10">
+          <v-app-bar dark color="primary">
+            <v-toolbar-title>Train a model</v-toolbar-title>
+          </v-app-bar>
+          <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="6" md="12">
+                  <v-text-field
+                    prepend-icon="mdi-rename-box"
+                    v-model="model_name"
+                    label="Name your model"
+                    required
+                    :rules="nameRules"
+                  ></v-text-field>
+                  <v-text-field
+                    prepend-icon="mdi-rename-box"
+                    v-model="target"
+                    label="Enter target variable name"
+                    required
+                    :rules="nameRules"
+                  ></v-text-field>
+                  <v-text-field
+                    prepend-icon="mdi-rename-box"
+                    v-model="test_size"
+                    label="Enter test size percentage(suggested 30)"
+                    required
+                    :rules="percentRules"
+                  ></v-text-field>
+                  <v-select
+                    prepend-icon="mdi-rename-box"
+                    v-model="select"
+                    :items="items"
+                    :rules="nameRules"
+                    label="Select algorithm"
+                    required
+                  ></v-select>
+                </v-col>
+                <!-- <v-col cols="12" sm="6" md="12">
+                      
+                </v-col>-->
+
+                <!-- <v-col cols="12" sm="6" md="12">
+                      
+                </v-col>-->
+
+                <v-col cols="12" sm="6" md="12">
+                  <v-file-input
+                    v-model="files"
+                    color="primary"
+                    counter
+                    accept="CSV/csv"
+                    label="Dataset (CSV file)"
+                    placeholder="Enter your dataset"
+                    prepend-icon="mdi-paperclip"
+                    outlined
+                    :show-size="1000"
+                    :rules="fileRules"
+                  >
+                    <template v-slot:selection="{ index, text }">
+                      <v-chip v-if="index < 2" color="primary accent-4" dark label small>{{ text }}</v-chip>
+
+                      <span
+                        v-else-if="index === 2"
+                        class="overline grey--text text--darken-3 mx-2"
+                      >+{{ files.length - 2 }} File(s)</span>
+                    </template>
+                  </v-file-input>
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" @click="reset()">Close</v-btn>
+              <v-btn color="blue darken-1" :disabled="!valid" @click="add()">Save</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="1">
+        <v-icon
+          v-show="display"
+          color="primary"
+          size="70"
+          style="margin-top: 200%; margin-left:20px;"
+        >mdi-arrow-right-bold-circle-outline</v-icon>
+      </v-col>
+      <v-col cols="12" lg="7">
+        <v-card class="mr-10 mt-10" v-show="display">
+          <v-app-bar dark color="primary">
+            <v-toolbar-title>Model metrics</v-toolbar-title>
+          </v-app-bar>
           <v-row>
-            <v-col cols="12" sm="6" md="12">
-              <v-text-field
-                prepend-icon="mdi-rename-box"
-                v-model="model_name"
-                label="Name your model"
-                required
-                :rules="nameRules"
-              ></v-text-field>
-              <v-text-field
-                prepend-icon="mdi-rename-box"
-                v-model="target"
-                label="Enter target variable name"
-                required
-                :rules="nameRules"
-              ></v-text-field>
-              <v-text-field
-                prepend-icon="mdi-rename-box"
-                v-model="test_size"
-                label="Enter test size percentage(suggested 30)"
-                required
-                :rules="percentRules"
-              ></v-text-field>
-              <v-select
-                prepend-icon="mdi-rename-box"
-                v-model="select"
-                :items="items"
-                :rules="nameRules"
-                label="Select algorithm"
-                required
-              ></v-select>
+            <v-col cols="12" md="6" lg="6" v-for="item in plots" :key="item">
+              <v-card class="ma-2" max-width="370">
+                <v-img :src="require(`../assets/${item.path}`)" height="300px"></v-img>
+
+                <v-card-title>{{item.title}}</v-card-title>
+
+                <v-card-subtitle>{{item.subtitle}}</v-card-subtitle>
+
+                <v-card-actions></v-card-actions>
+              </v-card>
+
+              <!-- <v-overlay absolute opacity="0.5" :value="overlay">
+                <v-img
+                  src="/home/sanfer/Documents/ml-examples-vuejs-flask/web-app/src/assets/matplotlib_histogram.png"
+                  height="300px"
+                ></v-img>
+                <v-btn
+                  class="mt-3"
+                  style="margin-left: 180px;"
+                  fab
+                  color="primary lighten-2"
+                  @click="overlay = !overlay"
+                >
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-overlay>-->
             </v-col>
-            <!-- <v-col cols="12" sm="6" md="12">
-                      
-            </v-col>-->
+            <v-col cols="12" md="6" lg="6">
+              <v-card class="ma-2" max-width="370">
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline mb-1">Error Metrics</v-list-item-title>
 
-            <!-- <v-col cols="12" sm="6" md="12">
-                      
-            </v-col>-->
-
-            <v-col cols="12" sm="6" md="12">
-              <v-file-input
-                v-model="files"
-                color="deep-purple accent-4"
-                counter
-                accept="CSV/csv"
-                label="Dataset (CSV file)"
-                placeholder="Enter your dataset"
-                prepend-icon="mdi-paperclip"
-                outlined
-                :show-size="1000"
-                :rules="fileRules"
-              >
-                <template v-slot:selection="{ index, text }">
-                  <v-chip
-                    v-if="index < 2"
-                    color="deep-purple accent-4"
-                    dark
-                    label
-                    small
-                    >{{ text }}</v-chip
-                  >
-
-                  <span
-                    v-else-if="index === 2"
-                    class="overline grey--text text--darken-3 mx-2"
-                    >+{{ files.length - 2 }} File(s)</span
-                  >
-                </template>
-              </v-file-input>
+                    <v-list-item v-for="item in metrics" :key="item" two-line>
+                      <v-list-item-content>
+                        <v-list-item-title>{{item.name}} = {{item.value}}</v-list-item-title>
+                        <v-list-item-subtitle>{{item.subtitle}}</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card>
             </v-col>
           </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" @click="reset()">Close</v-btn>
-          <v-btn color="blue darken-1" :disabled="!valid" @click="add()"
-            >Save</v-btn
-          >
-        </v-card-actions>
-      </v-form>
-    </v-card>
+
+          <v-card-actions class="mb-12">
+            <v-spacer></v-spacer>
+            <v-btn rounded color="primary" dark>Make Predictions</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
@@ -99,6 +158,8 @@ export default {
   name: "Home",
   components: {},
   data: () => ({
+    overlay: true,
+    show: false,
     nameRules: [v => !!v || "Title is required"],
     percentRules: [
       v => /^[1-9][0-9]?$|^100$/.test(v) || "Enter percentage (1-100%)"
@@ -116,7 +177,10 @@ export default {
     target: "",
     test_size: "",
     valid: true,
-    lazy: true
+    lazy: true,
+    plots: [],
+    metrics: [],
+    display: false
   }),
   methods: {
     add() {
@@ -135,6 +199,57 @@ export default {
           .then(response => {
             console.log(response.data.status);
             console.log(response.data.rmse);
+
+            // var data = {
+            //   scatterplotpath: response.data.ploturl["scatterplotpath"],
+            //   distpath: response.data.ploturl["distpath"],
+            //   residualpath: response.data.ploturl["residualpath"]
+            // };
+            // this.plots = data;-
+            var mae = {
+              name: "MAE",
+              subtitle: "Mean Absolute Error",
+              value: response.data.metrics["mae"]
+            };
+            this.metrics.push(mae);
+            var mse = {
+              name: "MSE",
+              subtitle: "Mean Squared Error",
+              value: response.data.metrics["mse"]
+            };
+            this.metrics.push(mse);
+            var rmse = {
+              name: "RMSE",
+              subtitle: "Root Mean Squared Error",
+              value: response.data.metrics["rmse"]
+            };
+            this.metrics.push(rmse);
+            var score = {
+              name: "Score",
+              subtitle: "R^2 value",
+              value: response.data.metrics["r_square"]
+            };
+            this.metrics.push(score);
+
+            var scatter = {
+              path: response.data.ploturl["scatterplotpath"],
+              title: "Scatter Plot",
+              subtitle: "Predicted vs Actual target variable"
+            };
+            this.plots.push(scatter);
+            var dist = {
+              path: response.data.ploturl["distpath"],
+              title: "Histogram",
+              subtitle: "Of target variable"
+            };
+            this.plots.push(dist);
+            var residual = {
+              path: response.data.ploturl["residualpath"],
+              title: "Residual Histogram",
+              subtitle: "Histogram of Actual-Predicted target variable"
+            };
+            this.plots.push(residual);
+            this.display = true;
           })
           .catch(error => {
             console.log(error);
@@ -169,6 +284,9 @@ export default {
           });
       } else if (this.select == "Bayesian Ridge Regression") {
         formData.set("name", this.model_name);
+        formData.set("target", this.target);
+        formData.set("test_size", this.test_size / 100);
+        formData.set("dataset", this.files);
         axios({
           method: "post",
           url: "/api/BayReg/",
@@ -177,6 +295,7 @@ export default {
         })
           .then(response => {
             console.log(response.data.status);
+            // console.log(response.data.rmse);
           })
           .catch(error => {
             console.log(error);
